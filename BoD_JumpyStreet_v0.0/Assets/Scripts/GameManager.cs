d2613797c7aa17947d9ab924f2f552c6274147be
zebbaukhagen <69488628+zebbaukhagen@users.noreply.github.com>
2024-02-04 16:15:19 -0500

    Add car materials, organized project, made GameManager class

---
+++ BoD_JumpyStreet_v0.0/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton pattern
+
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject gameManager = new("GameManager");
+                instance = gameManager.AddComponent<GameManager>();
+                DontDestroyOnLoad(gameManager);
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+
+    private int playerScore = 0;
+    private int highScore = 0;
+
+    public int HighScore
+    {
+        get => highScore;
+        private set => highScore = value;
+    }
+
+    public void IncrementPlayerScore()
+    {
+        playerScore++;
+        if (playerScore > HighScore) { HighScore = playerScore; }
+    }
+
+    // the rest of your code here
+}
